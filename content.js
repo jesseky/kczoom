fe9ba59763e83623c51aef66abee6d99e69765d1
杰西客 <microji@126.com>
2017-03-23 11:53:12 +0800

    improve performance

--- content.js
+++ content.js
@@ -7,34 +7,33 @@
 /* jshint esversion: 6 */
 
 // bind click image event
-var Querys = [
-  {
-    title: 'oschina',
+var Querys = [{
+    url: 'oschina.net',
     query: '.logs .tweet .img img, .UserLogs .photo img, .multimgs img',
     from: '_thumb',
     to: ''
   },
   {
-    title: 'oschina',
-    query: '.SmallPortrait,.LargePortrait,.tweet-vote-user img,.user-icon img, .tweet-portrait, .portrait, img.voter',
+    url: 'oschina.net',
+    query: 'img.SmallPortrait, img.LargePortrait, .tweet-vote-user img,.user-icon img, img.tweet-portrait, img.portrait, img.voter',
     from: /_\d+/,
     to: '_200',
     to2: '_100'
   },
   {
-    title: 'oschina',
+    url: 'oschina.net',
     query: 'img.multing',
     from: '(V)',
     to: ''
   },
   {
-    title: 'zhihu',
-    query: '.zhi .avatar, .zhi .Avatar, .zhi .zm-item-img-avatar, .avatar.avatar-small, .author .avatar-small, .Avatar',
+    url: 'zhihu.com',
+    query: '.zhi img.avatar, .zhi img.Avatar, .zhi img.side-topic-avatar, .zhi img.zm-item-img-avatar, img.avatar.avatar-small, .author img.avatar-small, img.Avatar',
     from: /_[msl]|_xs|_xl|_is|_im/,
     to: ''
   },
   {
-    title: 'douban',
+    url: 'douban.com',
     //query: '.user-face .pil, .obu .nbg .m_sub_img, #db-usr-profile .pic img, .basic-info .userface, .member-list .pic img.imgnoga, .comment-item .pic img, #friend .list img.show-title, .status-item .usr-pic a img, .mod-usercard .pic a img',
     query: 'img[src*=".doubanio.com/icon/u"]:not(.kcimage-content-img)',
     from: /icon\/u[a-z]?/,
@@ -43,21 +42,26 @@ var Querys = [
   }
 ];
 var ImgCon = null;
+var CurrentURL = document.location.href;
+var MutationOb = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver; // moz is not required
+
 function kcHideSourceImage() {
   ImgCon.classList.add('kc-fadeOut');
-  setTimeout(function() {
+  setTimeout(function () {
     ImgCon.classList.remove('kc-fadeOut');
     ImgCon.style.display = 'none';
   }, 600);
 }
+
 function kcHideOrShowLoading(isShow) {
   if (ImgCon) {
     ImgCon.querySelector('.kcimage-loading').style.display = typeof isShow !== 'undefined' && isShow ? 'flex' : 'none';
   }
 }
+
 function kcLoadImage(ImgCon, img, source, callback, callerror) {
   var load = new Image();
-  load.onload = function() {
+  load.onload = function () {
     kcHideOrShowLoading();
     img.setAttribute('src', source);
     img.classList.add('kcimage-zoom-out');
@@ -66,20 +70,21 @@ function kcLoadImage(ImgCon, img, source, callback, callerror) {
       ImgCon.querySelector('section').classList.add('kcimage-content-cont-block');
       ImgCon.querySelector('section').scrollTop = (h - ImgCon.offsetHeight) / 2;
     }
-    setTimeout(function() {
+    setTimeout(function () {
       img.classList.add('kc-bounceIn');
     }, 0);
     if (typeof callback === 'function') {
       callback();
     }
   };
-  load.onerror = function() {
+  load.onerror = function () {
     if (typeof callerror === 'function') {
       callerror();
     }
   };
   load.src = source;
 }
+
 function kcLoadSourceImage(elm, iq) {
   var img;
   if (!ImgCon) {
@@ -89,15 +94,15 @@ function kcLoadSourceImage(elm, iq) {
     ImgCon.innerHTML = '<div class="kcimage-content-in"><b class="kcimage-content-close">×</b><div class="kcimage-loading"></div><section class="kcimage-content-cont"></section></div>';
     document.body.appendChild(ImgCon);
     ImgCon.querySelector('.kcimage-loading').innerHTML = '<div class="kc-loading"></div>';
-    ImgCon.querySelector('.kcimage-content-close').addEventListener('click', function(evt) {
+    ImgCon.querySelector('.kcimage-content-close').addEventListener('click', function (evt) {
       kcHideSourceImage();
     }, false);
-    ImgCon.querySelector('section').addEventListener('click', function(evt) {
+    ImgCon.querySelector('section').addEventListener('click', function (evt) {
       if (evt.target === ImgCon.querySelector('img')) {
         kcHideSourceImage();
       }
     }, false);
-    ImgCon.addEventListener('mousewheel', function(evt) { // DOMMouseScroll , -evt.detail * 40
+    ImgCon.addEventListener('mousewheel', function (evt) { // DOMMouseScroll , -evt.detail * 40
       ImgCon.querySelector('section').scrollTop -= evt.wheelDeltaY;
       evt.preventDefault();
       evt.stopPropagation();
@@ -114,13 +119,13 @@ function kcLoadSourceImage(elm, iq) {
   ImgCon.style.display = 'block';
   img.src = elm.src;
   img.classList.remove('kcimage-zoom-out');
-  kcLoadImage(ImgCon, img, iq.from ? elm.src.replace(iq.from, iq.to || '') : elm.src, function() {
+  kcLoadImage(ImgCon, img, iq.from ? elm.src.replace(iq.from, iq.to || '') : elm.src, function () {
     // success;
-  }, function() {
+  }, function () {
     if (iq.from && typeof iq.to2 !== 'undefined') {
-      kcLoadImage(ImgCon, img, elm.src.replace(iq.from, iq.to2), function() {
+      kcLoadImage(ImgCon, img, elm.src.replace(iq.from, iq.to2), function () {
         //success;
-      }, function() {
+      }, function () {
         kcHideOrShowLoading();
       });
     } else {
@@ -128,14 +133,14 @@ function kcLoadSourceImage(elm, iq) {
     }
   });
 }
+
 function kcBindAllEvents() {
-  Querys.forEach(function(iq) {
-    [...document.querySelectorAll(iq.query)].filter(el => !el.kcBoundEvent).forEach(function(elm) {
-      let img = elm.nodeName.toUpperCase() === 'IMG' ? elm : elm.querySelector('img');
+  Querys.filter(iq => CurrentURL.indexOf(iq.url) > -1).forEach(function (iq) {
+    [...document.querySelectorAll(iq.query.split(',').map(i=>i+":not(.kcimage-zoom)").join(','))].forEach(function (img) {
       img.kcBoundEvent = true;
       img.style.cursor = 'zoom-in';
       img.classList.add('kcimage-zoom');
-      img.addEventListener('click', function(evt) {
+      img.addEventListener('click', function (evt) {
         kcLoadSourceImage(img, iq);
         evt.preventDefault();
         evt.stopPropagation();
@@ -145,7 +150,14 @@ function kcBindAllEvents() {
 }
 kcBindAllEvents();
 
-// content changed
-document.addEventListener('DOMSubtreeModified', function() {
-  kcBindAllEvents();
-}, false);
+if (MutationOb) {
+  var mo = new MutationOb(kcBindAllEvents);
+  mo.observe(document.body, {
+    'childList': true,
+    'subtree': true
+  });
+} else {
+  document.addEventListener('DOMNodeInserted', function () { // content changed
+    kcBindAllEvents();
+  }, false);
+}
